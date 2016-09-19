7724693f3df9b2c35e980aa21400d34406fde65f
weihanli <ben121011@126.com>
2016-09-20 01:29:33 +0800

    PagedListModel BUG紧急修复

--- ActivityReservation.AdminLogic/Helpers/PagedListModel.cs
+++ ActivityReservation.AdminLogic/Helpers/PagedListModel.cs
@@ -8,28 +8,39 @@ namespace ActivityReservation.Helpers
     /// 数据分页模型
     /// </summary>
     /// <typeparam name="T">数据类型</typeparam>
-    public class PagedListModel<T> where T : class, new()
+    public class PagedListModel<T>
     {
-        public List<T> Data { get; set; }
+        public IEnumerable<T> Data { get; set; }
 
         public PagerModel Pager { get; set; }
+
+        public PagedListModel(IEnumerable<T> data)
+        {
+            Data = data;
+        }
+
+        public PagedListModel(IEnumerable<T> data, PagerModel pager)
+        {
+            Data = data;
+            Pager = pager;
+        }
     }
 
     public static class Extensions
     {
-        public static PagedList<T> ToPagedList<T>(this IEnumerable<T> data)
+        public static PagedListModel<T> ToPagedList<T>(this IEnumerable<T> data)
         {
-            return new PagedList<T>(data);
+            return new PagedListModel<T>(data);
         }
 
-        public static PagedList<T> ToPagedList<T>(this IEnumerable<T> data,PagerModel pager)
+        public static PagedListModel<T> ToPagedList<T>(this IEnumerable<T> data,PagerModel pager)
         {
-            return new PagedList<T>(data,pager);
+            return new PagedListModel<T>(data,pager);
         }
 
-        public static PagedList<T> ToPagedList<T>(this IEnumerable<T> data,int pageIndex,int pageSize,int totalCount)
+        public static PagedListModel<T> ToPagedList<T>(this IEnumerable<T> data,int pageIndex,int pageSize,int totalCount)
         {
-            return new PagedList<T>(data,new PagerModel(pageIndex,pageSize,totalCount));
+            return new PagedListModel<T>(data,new PagerModel(pageIndex,pageSize,totalCount));
         }
     }
 }
