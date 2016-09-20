3cb6797e28e6c09355a07dbbb8e97fb0dc99eefe
weihanli <ben121011@126.com>
2016-09-21 01:08:29 +0800

    自定义404错误页，删除无用的Contact页面

---
+++ ActivityReservation/Controllers/ErrorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActivityReservation.Controllers
+{
+    public class ErrorController : Controller
+    {
+        /// <summary>
+        /// 404 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult NotFound(string aspxerrorpath)
+        {
+            //Response.StatusCode = 404;
+            return View();
+        }
+    }
+}
--- ActivityReservation/Controllers/HomeController.cs
+++ ActivityReservation/Controllers/HomeController.cs
@@ -230,14 +230,6 @@ namespace ActivityReservation.Controllers
         }
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
