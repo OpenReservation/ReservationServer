064f5f513d89652b59fd9d670291d9b95313974a
weihanli <ben121011@126.com>
2016-10-18 10:16:53 +0800

    补提交T4模板生成的文件

---
+++ Business/BLLCollection.generated.cs
@@ -0,0 +1,62 @@
+﻿ 
+using DataAccess;
+using Models;
+namespace Business
+{
+	public partial class BLLUser : BaseBLL<User>
+    {
+        protected override void InitDbHandler()
+        {
+            dbHandler = new DALUser();
+        }
+    }
+	public partial class BLLBlockType : BaseBLL<BlockType>
+    {
+        protected override void InitDbHandler()
+        {
+            dbHandler = new DALBlockType();
+        }
+    }
+	public partial class BLLBlockEntity : BaseBLL<BlockEntity>
+    {
+        protected override void InitDbHandler()
+        {
+            dbHandler = new DALBlockEntity();
+        }
+    }
+	public partial class BLLOperationLog : BaseBLL<OperationLog>
+    {
+        protected override void InitDbHandler()
+        {
+            dbHandler = new DALOperationLog();
+        }
+    }
+	public partial class BLLReservation : BaseBLL<Reservation>
+    {
+        protected override void InitDbHandler()
+        {
+            dbHandler = new DALReservation();
+        }
+    }
+	public partial class BLLReservationPlace : BaseBLL<ReservationPlace>
+    {
+        protected override void InitDbHandler()
+        {
+            dbHandler = new DALReservationPlace();
+        }
+    }
+	public partial class BLLSystemSettings : BaseBLL<SystemSettings>
+    {
+        protected override void InitDbHandler()
+        {
+            dbHandler = new DALSystemSettings();
+        }
+    }
+	public partial class BLLNotice : BaseBLL<Notice>
+    {
+        protected override void InitDbHandler()
+        {
+            dbHandler = new DALNotice();
+        }
+    }
+}
---
+++ DataAccess/DALCollection.generated.cs
@@ -0,0 +1,13 @@
+﻿ 
+using Models;
+namespace DataAccess
+{
+	public partial class DALUser: BaseDaL<User> { }
+	public partial class DALBlockType: BaseDaL<BlockType> { }
+	public partial class DALBlockEntity: BaseDaL<BlockEntity> { }
+	public partial class DALOperationLog: BaseDaL<OperationLog> { }
+	public partial class DALReservation: BaseDaL<Reservation> { }
+	public partial class DALReservationPlace: BaseDaL<ReservationPlace> { }
+	public partial class DALSystemSettings: BaseDaL<SystemSettings> { }
+	public partial class DALNotice: BaseDaL<Notice> { }
+}
