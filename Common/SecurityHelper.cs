509e9456d227e987bfb8628e6d0f7122cf50b335
weihanli <ben121011@126.com>
2016-10-29 14:50:19 +0800

    SecurityHelper 重构

--- Common/SecurityHelper.cs
+++ Common/SecurityHelper.cs
@@ -10,45 +10,36 @@ namespace Common
 {
     public class SecurityHelper
     {
+
+        public static byte[] MD5_Hash(byte[] sourceBytes)
+        {
+            return HashHelper.GetHashedBytes(HashType.MD5, sourceBytes);
+        }
+
         /// <summary>
         /// MD5加密
         /// </summary>
-        /// <param name="strPwd">原字符串</param>
+        /// <param name="sourceString">原字符串</param>
         /// <returns>加密后字符串</returns>
-        public static string GetMD5(string strPwd)
+        public static string MD5_Encrypt(string sourceString, bool isLower = false)
         {
-            //MD5 对象创建的两种方式
-            //MD5 md5 = MD5.Create();
-            MD5 md5 = new MD5CryptoServiceProvider();
-            //将输入的密码转换成字节数组
-            byte[] bPwd = Encoding.UTF8.GetBytes(strPwd);    
-            //计算指定字节数组的哈希值
-            byte[] bMD5 = md5.ComputeHash(bPwd);
-            //释放加密服务提供类的所有资源
-            md5.Clear();   
-            StringBuilder sbMD5Pwd = new StringBuilder();
-            for (int i = 0; i < bMD5.Length; i++)
+            if (String.IsNullOrEmpty(sourceString))
             {
-                //将每个字节数据转换为2位的16进制的字符
-                sbMD5Pwd.Append(bMD5[i].ToString("X2"));
+                return "";
             }
-            return sbMD5Pwd.ToString();
+            return HashHelper.GetHashedString(HashType.MD5, sourceString, isLower);
         }
 
         /// <summary>
         /// use sha1 to encrypt string
         /// </summary>
-        public static string SHA1_Encrypt(string Source_String)
+        public static string SHA1_Encrypt(string sourceString, bool isLower = false)
         {
-            byte[] StrRes = Encoding.UTF8.GetBytes(Source_String);
-            HashAlgorithm iSHA = new SHA1CryptoServiceProvider();
-            StrRes = iSHA.ComputeHash(StrRes);
-            StringBuilder EnText = new StringBuilder();
-            foreach (byte iByte in StrRes)
+            if (String.IsNullOrEmpty(sourceString))
             {
-                EnText.AppendFormat("{0:x2}", iByte);
+                return "";
             }
-            return EnText.ToString();
+            return HashHelper.GetHashedString(HashType.MD5, sourceString, isLower);
         }
 
         /// <summary>
@@ -56,17 +47,13 @@ namespace Common
         /// </summary>
         /// <param name="sourceString"></param>
         /// <returns></returns>
-        public static string SHA256_Encrypt(string sourceString)
+        public static string SHA256_Encrypt(string sourceString, bool isLower = false)
         {
-            byte[] data = Encoding.UTF8.GetBytes(sourceString);
-            SHA256 shaM = SHA256.Create();
-            byte[] result = shaM.ComputeHash(data);
-            StringBuilder EnText = new StringBuilder();
-            foreach (byte iByte in result)
+            if (String.IsNullOrEmpty(sourceString))
             {
-                EnText.AppendFormat("{0:x2}", iByte);
+                return "";
             }
-            return EnText.ToString();
+            return HashHelper.GetHashedString(HashType.MD5, sourceString, isLower);
         }
 
         /// <summary>
@@ -74,17 +61,13 @@ namespace Common
         /// </summary>
         /// <param name="sourceString"></param>
         /// <returns></returns>
-        public static string SHA384_Encrypt(string sourceString)
+        public static string SHA384_Encrypt(string sourceString, bool isLower = false)
         {
-            byte[] data = Encoding.UTF8.GetBytes(sourceString);
-            SHA384 shaM = SHA384.Create();
-            byte[] result = shaM.ComputeHash(data);
-            StringBuilder EnText = new StringBuilder();
-            foreach (byte iByte in result)
+            if (String.IsNullOrEmpty(sourceString))
             {
-                EnText.AppendFormat("{0:x2}", iByte);
+                return "";
             }
-            return EnText.ToString();
+            return HashHelper.GetHashedString(HashType.MD5, sourceString, isLower);
         }
 
         /// <summary>
@@ -92,17 +75,13 @@ namespace Common
         /// </summary>
         /// <param name="sourceString"></param>
         /// <returns></returns>
-        public static string SHA512_Encrypt(string sourceString)
+        public static string SHA512_Encrypt(string sourceString, bool isLower = false)
         {
-            byte[] data = Encoding.UTF8.GetBytes(sourceString);
-            SHA512 shaM = new SHA512Managed();
-            byte[] result = shaM.ComputeHash(data);
-            StringBuilder EnText = new StringBuilder();
-            foreach (byte iByte in result)
+            if (String.IsNullOrEmpty(sourceString))
             {
-                EnText.AppendFormat("{0:x2}", iByte);
+                return "";
             }
-            return EnText.ToString();
+            return HashHelper.GetHashedString(HashType.MD5, sourceString, isLower);
         }
 
         /// <summary>
@@ -112,7 +91,7 @@ namespace Common
         /// <returns></returns>
         public static string MulEncrypt(string str)
         {
-            return SHA384_Encrypt(GetMD5(str));
+            return SHA384_Encrypt(MD5_Encrypt(str));
         }
 
         #region 1.0 使用 票据对象 将 用户数据 加密成字符串 +string EncryptUserInfo(string userInfo)
@@ -371,5 +350,101 @@ namespace Common
         }
 
         #endregion
+    }
+
+    static class HashHelper
+    {
+        /// <summary>
+        /// 计算字符串Hash值
+        /// </summary>
+        /// <param name="type">hash类型</param>
+        /// <param name="str">要hash的字符串</param>
+        /// <returns>hash过的字节数组</returns>
+        public static byte[] GetHashedBytes(HashType type, string str) => GetHashedBytes(type, str, Encoding.UTF8);
+
+        /// <summary>
+        /// 计算字符串Hash值
+        /// </summary>
+        /// <param name="type">hash类型</param>
+        /// <param name="str">要hash的字符串</param>
+        /// <param name="encoding">编码类型</param>
+        /// <returns>hash过的字节数组</returns>
+        public static byte[] GetHashedBytes(HashType type, string str, Encoding encoding)
+        {
+            byte[] bytes = encoding.GetBytes(str);
+            return GetHashedBytes(type, bytes);
+        }
+
+        public static string GetHashedString(HashType type, string str) => GetHashedString(type, str, Encoding.UTF8);
+
+        public static string GetHashedString(HashType type, string str, bool isLower) => GetHashedString(type, str, Encoding.UTF8, isLower);
+
+        public static string GetHashedString(HashType type, string str, Encoding encoding, bool isLower = false)
+        {
+            byte[] hashedBytes = GetHashedBytes(type, str, encoding);
+            StringBuilder sbText = new StringBuilder();
+            if (isLower)
+            {
+                foreach (byte b in hashedBytes)
+                {
+                    sbText.Append(b.ToString("x2"));
+                }
+            }
+            else
+            {
+                foreach (byte b in hashedBytes)
+                {
+                    sbText.Append(b.ToString("X2"));
+                }
+            }
+            return sbText.ToString();
+        }
+
+        /// <summary>
+        /// 获取Hash后的字节数组
+        /// </summary>
+        /// <param name="type">哈希类型</param>
+        /// <param name="bytes">原字节数组</param>
+        /// <returns></returns>
+        public static byte[] GetHashedBytes(HashType type, byte[] bytes)
+        {
+            HashAlgorithm algorithm;
+            switch (type)
+            {
+                case HashType.MD5:
+                    algorithm = MD5.Create();
+                    break;
+                case HashType.SHA1:
+                    algorithm = SHA1.Create();
+                    break;
+                case HashType.SHA256:
+                    algorithm = SHA256.Create();
+                    break;
+                case HashType.SHA384:
+                    algorithm = SHA384.Create();
+                    break;
+                case HashType.SHA512:
+                    algorithm = SHA512.Create();
+                    break;
+                default:
+                    algorithm = MD5.Create();
+                    break;
+            }
+            byte[] hashedBytes = algorithm.ComputeHash(bytes);
+            algorithm.Dispose();
+            return hashedBytes;
+        }
+    }
+
+    /// <summary>
+    /// Hash 类型
+    /// </summary>
+    public enum HashType
+    {
+        MD5 = 0,
+        SHA1 = 1,
+        SHA256 = 2,
+        SHA384 = 3,
+        SHA512 = 4
     }
 }
