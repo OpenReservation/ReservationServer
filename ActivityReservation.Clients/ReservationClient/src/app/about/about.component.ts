f95b0d0ca5dd1af88c3a0dca529944326244b4b6
WeihanLi <weihanli@outlook.com>
2019-06-24 17:08:14 +0800

    disable loading for about

--- ActivityReservation.Clients/ReservationClient/src/app/about/about.component.ts
+++ ActivityReservation.Clients/ReservationClient/src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingService } from '../services/LoadingService';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loadingSvc: LoadingService) { 
+    this.loadingSvc.isLoading = false;
+  }
 
   ngOnInit() {
+    
   }
 
 }
