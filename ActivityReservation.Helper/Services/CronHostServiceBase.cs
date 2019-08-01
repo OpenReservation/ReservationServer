e1f735f38e7dc9d80ff49b531205a241caf9200f
WeihanLi <weihanli@outlook.com>
2019-08-01 10:01:43 +0800

    add RedLock

--- ActivityReservation.Helper/Services/CronHostServiceBase.cs
+++ ActivityReservation.Helper/Services/CronHostServiceBase.cs
@@ -4,12 +4,15 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using WeihanLi.Common.Helpers;
+using WeihanLi.Redis;
 
 namespace ActivityReservation.Services
 {
     public abstract class CronHostServiceBase : BackgroundService
     {
         public abstract string CronExpression { get; }
+
+        public bool ConcurrentAllowed { get; set; }
 
         protected readonly ILogger Logger;
 
@@ -27,18 +30,36 @@ namespace ActivityReservation.Services
             {
                 var now = DateTimeOffset.UtcNow;
 
-                if (now > next)
+                if (now >= next)
                 {
-                    await ProcessAsync().ConfigureAwait(false);
-                    next = CronHelper.GetNextOccurrence(CronExpression);
-                    Logger.LogInformation($"Next at {next.Value.DateTime.ToLongDateString()} {next.Value.DateTime.ToLongTimeString()}");
+                    if (ConcurrentAllowed)
+                    {
+                        _ = ProcessAsync();
+                    }
+                    else
+                    {
+                        using (var locker = RedisManager.GetRedLockClient($"{GetType()}_cronService"))
+                        {
+                            if (await locker.TryLockAsync())
+                            {
+                                await ProcessAsync();
+
+                                next = CronHelper.GetNextOccurrence(CronExpression);
+                                Logger.LogInformation($"Next at {next.Value.DateTime.ToLongDateString()} {next.Value.DateTime.ToLongTimeString()}");
+                            }
+                            else
+                            {
+                                Logger.LogInformation($"failed to acquire lock");
+                            }
+                        }
+                    }
                 }
                 else
                 {
                     // needed for graceful shutdown for some reason.
                     // 100ms chosen so it doesn't affect calculating the next
                     // cron occurence (lowest possible: every second)
-                    await Task.Delay(100, stoppingToken);
+                    await Task.Delay(500, stoppingToken);
                 }
             }
         }
