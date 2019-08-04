43ec75b8b5583146a71b3a7bcac91fb662642fa7
weihanli <weihanli@outlook.com>
2019-08-04 22:54:31 +0800

    update scheduleService

--- ActivityReservation.Helper/Services/CronScheduleServiceBase.cs
+++ ActivityReservation.Helper/Services/CronScheduleServiceBase.cs
@@ -93,4 +93,58 @@ namespace ActivityReservation.Services
             return base.StopAsync(cancellationToken);
         }
     }
+
+    public abstract class TimerScheduledService : IHostedService, IDisposable
+    {
+        private readonly Timer _timer;
+        private readonly TimeSpan _period;
+        protected readonly ILogger Logger;
+
+        protected TimerScheduledService(TimeSpan period, ILogger logger)
+        {
+            Logger = logger;
+            _period = period;
+            _timer = new Timer(Execute, null, Timeout.Infinite, 0);
+        }
+
+        public void Execute(object state = null)
+        {
+            try
+            {
+                Logger.LogInformation("Begin execute service");
+                ExecuteAsync().Wait();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, "Execute exception");
+            }
+            finally
+            {
+                Logger.LogInformation("Execute finished");
+            }
+        }
+
+        protected abstract Task ExecuteAsync();
+
+        public virtual void Dispose()
+        {
+            _timer?.Dispose();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Logger.LogInformation("Service is starting.");
+            _timer.Change(TimeSpan.FromSeconds(SecurityHelper.Random.Next(10)), _period);
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Logger.LogInformation("Service is stopping.");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+    }
 }
--- ActivityReservation.Helper/Services/RemoveOverdueReservationService.cs
+++ ActivityReservation.Helper/Services/RemoveOverdueReservationService.cs
@@ -5,67 +5,11 @@ using ActivityReservation.Database;
 using ActivityReservation.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using WeihanLi.Common.Helpers;
 using WeihanLi.EntityFramework;
 
 namespace ActivityReservation.Services
 {
-    public abstract class TimerScheduledService : IHostedService, IDisposable
-    {
-        private readonly Timer _timer;
-        private readonly TimeSpan _period;
-        protected readonly ILogger Logger;
-
-        protected TimerScheduledService(TimeSpan period, ILogger logger)
-        {
-            Logger = logger;
-            _period = period;
-            _timer = new Timer(Execute, null, Timeout.Infinite, 0);
-        }
-
-        public void Execute(object state = null)
-        {
-            try
-            {
-                Logger.LogInformation("Begin execute service");
-                ExecuteAsync().Wait();
-            }
-            catch (Exception ex)
-            {
-                Logger.LogError(ex, "Execute exception");
-            }
-            finally
-            {
-                Logger.LogInformation("Execute finished");
-            }
-        }
-
-        protected abstract Task ExecuteAsync();
-
-        public virtual void Dispose()
-        {
-            _timer?.Dispose();
-        }
-
-        public Task StartAsync(CancellationToken cancellationToken)
-        {
-            Logger.LogInformation("Service is starting.");
-            _timer.Change(TimeSpan.FromSeconds(SecurityHelper.Random.Next(10)), _period);
-            return Task.CompletedTask;
-        }
-
-        public Task StopAsync(CancellationToken cancellationToken)
-        {
-            Logger.LogInformation("Service is stopping.");
-
-            _timer?.Change(Timeout.Infinite, 0);
-
-            return Task.CompletedTask;
-        }
-    }
-
     public class RemoveOverdueReservationService : CronScheduleServiceBase
     {
         private readonly IServiceProvider _serviceProvider;
