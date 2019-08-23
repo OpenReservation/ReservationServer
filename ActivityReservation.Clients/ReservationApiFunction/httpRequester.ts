6c61086dff1a3c7dd9bbd793c91bc2c9e701bcd2
WeihanLi <weihanli@outlook.com>
2019-08-23 21:20:38 +0800

    add ReservationApiFunction

---
+++ ActivityReservation.Clients/ReservationApiFunction/httpRequester.ts
@@ -0,0 +1,46 @@
+import { IncomingHttpHeaders } from "http";
+import * as got from 'got';
+
+export class HttpRequester{    
+    static async GetResponse(url:string, method:string, headers:IncomingHttpHeaders|undefined, body?:string): Promise<HttpResponse> {
+        let result = await got.default(url,{
+            body: body,
+            method: method
+        });
+        var response = new HttpResponse();
+        response.statusCode = result.statusCode;
+        response.responseText = result.body;
+        response.responseHeaders = result.headers;
+
+        return response;
+    }
+}
+
+export class HttpResponse{
+    
+    private _statusCode : number = -1;
+    public get statusCode() : number {
+        return this._statusCode;
+    }
+    public set statusCode(v : number) {
+        this._statusCode = v;
+    }
+    
+    
+    private _responseText : string = "";
+    public get responseText() : string {
+        return this._responseText;
+    }
+    public set responseText(v : string) {
+        this._responseText = v;
+    }
+    
+    private _headers : IncomingHttpHeaders|undefined = undefined;
+    public get responseHeaders() : IncomingHttpHeaders|undefined {
+        return this._headers;
+    }
+    public set responseHeaders(v : IncomingHttpHeaders|undefined) {
+        this._headers = v;
+    }
+    
+}
---
+++ ActivityReservation.Clients/ReservationApiFunction/index.ts
@@ -0,0 +1,50 @@
+import { HttpRequester } from './httpRequester';
+
+const apiBaseUrl = "https://reservation.weihanli.xyz";
+
+exports.main_handler = async function (event: any, context: any, callback: any): Promise<object> {
+
+    let queryString = "";
+    if (event.queryString) {
+        queryString = "?";
+        let i = 0;
+        for (var key in event.queryString) {
+            if (i > 0) {
+                queryString += `&`;
+            } else {
+                i++;
+            }
+            queryString += `${key}=${event.queryString[key]}`;
+        }
+    }
+
+    if((<string>event.path).startsWith('/reservationWxAppGateway')){
+        event.path = (<string>event.path).replace('/reservationWxAppGateway', '');
+    }
+
+    let url = `${apiBaseUrl}${event.path}${queryString}`;
+    console.log(`request url: ${url}, method: ${event.httpMethod}`);
+    let response = await HttpRequester.GetResponse(url, event.httpMethod, event.headers, event.body);
+
+    return {
+        isBase64: false,
+        statusCode: response.statusCode,
+        headers: response.responseHeaders,
+        body: response.responseText
+    };
+}
+
+// (async () => {
+//   var event = new Event();
+//   event.path = "/api/notice";
+//   event.headers = {
+//       "X-A": "AAA"
+//   };
+//   event.httpMethod = "GET";
+//   event.queryString = {
+//       pageNumber: 1
+//   };
+
+//   var response = await main(event,null, null);
+//   console.log(response);
+// })();
