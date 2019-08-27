d0f4d80950cb99b0fc3389ff6732677cd2007cc6
weihanli <weihanli@outlook.com>
2019-08-27 23:56:32 +0800

    update test code

--- ActivityReservation.Clients/ReservationApiFunction/index.ts
+++ ActivityReservation.Clients/ReservationApiFunction/index.ts
@@ -40,17 +40,19 @@ async function main (event: any, context: any, callback: any): Promise<object> {
 exports.main_handler = main;
 
 // (async () => {
+//   process.env.NODE_TLS_REJECT_UNAUTHORIZED="0";
 //   var event:any = new Object();
 //   event.path = "/api/reservation";
 //   event.headers = {
-//     "Content-Type": "application/json",
+//     "Content-Type": "application/json;charset=utf-8",
+//     "X-Query": "abccd",
+//     "User-Agent": "Chrome 79"
 //   };
 //   event.httpMethod = "GET";
 //   event.body = '{"ReservationPlaceId":"6bbbcfa9-7b38-444d-8a80-5da9d06c62a7","ReservationPlaceName":"多媒体工作室","ReservationForDate":"2019-08-27","ReservationForTimeIds":"0","ReservationForTime":"08:00-10:00","ReservationUnit":"测试","ReservationActivityContent":"测测更健康","ReservationPersonName":"测试001","ReservationPersonPhone":"13222223333"}';
 //   event.queryString = {
 //       pageNumber: 1
 //   };
-
-//   var response = await main(event,null, null);
+//   var response = await main(event, null, null);
 //   console.log(response);
 // })();
