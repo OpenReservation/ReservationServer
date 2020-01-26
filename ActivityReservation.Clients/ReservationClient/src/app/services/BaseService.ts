ac37cbf65f1309b73d0a8c3f7eb1d21e12acef04
weihanli <weihanli@outlook.com>
2020-01-26 11:27:16 +0800

    clean code

--- ActivityReservation.Clients/ReservationClient/src/app/services/BaseService.ts
+++ ActivityReservation.Clients/ReservationClient/src/app/services/BaseService.ts
@@ -1,4 +1,3 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { PagedListData } from '../models/PagedListData';
