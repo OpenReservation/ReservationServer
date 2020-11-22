9e48ddbd3174b15103eb60b29449a05383cb2bc3
weihanli <weihanli@outlook.com>
2020-11-22 17:32:57 +0800

    integrate DotNetRuntime metrics

--- OpenReservation/Program.cs
+++ OpenReservation/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Prometheus.DotNetRuntime;
 
 namespace OpenReservation
 {
@@ -8,6 +9,8 @@ namespace OpenReservation
     {
         public static void Main(string[] args)
         {
+            DotNetRuntimeStatsBuilder.Default().StartCollecting();
+
             Host.CreateDefaultBuilder(args)
                .ConfigureAppConfiguration((context, builder) =>
                {
