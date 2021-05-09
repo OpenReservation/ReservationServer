203de176a50c1dc4f55c2a4ca38ebc1213e4811f
weihanli <weihanli@outlook.com>
2021-05-09 13:41:29 +0800

    get model types from the compilation

--- OpenReservation.Database/ServiceGenerator.cs
+++ OpenReservation.Database/ServiceGenerator.cs
@@ -1,5 +1,7 @@
-﻿using System.Text;
+﻿using System.Linq;
+using System.Text;
 using Microsoft.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
 
 namespace OpenReservation.Database
 {
@@ -8,21 +10,34 @@ namespace OpenReservation.Database
     {
         public void Initialize(GeneratorInitializationContext context)
         {
+            // Debugger.Launch();
         }
 
         public void Execute(GeneratorExecutionContext context)
         {
-            var types = new[]{
-                "BlockType",
-                "BlockEntity",
-                "OperationLog",
-                "Reservation",
-                "ReservationPlace",
-                "ReservationPeriod",
-                "SystemSettings",
-                "Notice",
-                "DisabledPeriod"
-            };
+            var dbContextType = context.Compilation.GetTypeByMetadataName(typeof(DbSet<>).FullName);
+            var reservationDbContextType = context.Compilation.GetTypeByMetadataName(typeof(ReservationDbContext).FullName);
+            var propertySymbols = reservationDbContextType!.GetMembers()
+                .OfType<IMethodSymbol>()
+                .Where(x => x.IsVirtual
+                        && x.MethodKind == MethodKind.PropertyGet
+                        && x.ReturnType is INamedTypeSymbol
+                        {
+                            IsGenericType: true,
+                            IsUnboundGenericType: false,
+                        } typeSymbol
+                        && ReferenceEquals(typeSymbol.ConstructedFrom.ContainingAssembly, dbContextType!.ContainingAssembly)
+                        )
+                .ToArray()
+                ;
+            var propertyReturnType = propertySymbols
+                .Select(r => ((INamedTypeSymbol)r.ReturnType))
+                .ToArray();
+            var modelTypeNames = propertyReturnType
+                .Select(t => t.TypeArguments)
+                .SelectMany(x => x)
+                .Select(x => x.Name)
+                .ToArray();
 
             var codeBuilder = new StringBuilder();
             codeBuilder.AppendLine(@"
@@ -32,7 +47,7 @@ using WeihanLi.EntityFramework;
 
 namespace OpenReservation.Business
 {");
-            foreach (var item in types)
+            foreach (var item in modelTypeNames)
             {
                 codeBuilder.AppendLine($@"
     public partial interface IBLL{item}: IEFRepository<ReservationDbContext, {item}>{{}}
