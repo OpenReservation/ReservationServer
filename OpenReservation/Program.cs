ffb9cac28402acdd4dfb19f1d0f4c938dd056469
weihanli <weihanli@outlook.com>
2021-12-03 22:40:11 +0800

    Configure json logging encoder and user Top-Level statement

--- OpenReservation/Program.cs
+++ OpenReservation/Program.cs
@@ -3,37 +3,35 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using OpenReservation;
 using Prometheus.DotNetRuntime;
 
-namespace OpenReservation
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            DotNetRuntimeStatsBuilder.Default().StartCollecting();
+DotNetRuntimeStatsBuilder.Default().StartCollecting();
 
-            Host.CreateDefaultBuilder(args)
-               .ConfigureAppConfiguration(builder =>
-               {
-                   builder.AddEnvironmentVariables("Reservation_");
-               })
-               .ConfigureLogging(builder => builder.AddJsonConsole())
-               .ConfigureServices(services =>
-               {
-                   // prometheus counters metrics
-                   services.AddPrometheusCounters();
-                   services.AddPrometheusAspNetCoreMetrics();
-                   services.AddPrometheusSqlClientMetrics();
-                   services.AddPrometheusHttpClientMetrics();
-               })
-               .ConfigureWebHostDefaults(webHostBuilder =>
-               {
-                   webHostBuilder.UseStartup<Startup>();
-               })
-               .Build()
-               .Run()
-               ;
-        }
-    }
-}
+Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(builder =>
+    {
+        builder.AddEnvironmentVariables("Reservation_");
+    })
+    .ConfigureLogging(builder => builder.AddJsonConsole(options =>
+    {
+        options.JsonWriterOptions = new System.Text.Json.JsonWriterOptions()
+        {
+            Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+        };
+    }))
+    .ConfigureServices(services =>
+    {
+        // prometheus counters metrics
+        services.AddPrometheusCounters();
+        services.AddPrometheusAspNetCoreMetrics();
+        services.AddPrometheusSqlClientMetrics();
+        services.AddPrometheusHttpClientMetrics();
+    })
+    .ConfigureWebHostDefaults(webHostBuilder =>
+    {
+        webHostBuilder.UseStartup<Startup>();
+    })
+    .Build()
+    .Run()
+    ;
